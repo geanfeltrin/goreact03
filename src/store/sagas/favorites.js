9ee11b1e7de43676bc1e988442e228204ed65171
geanfeltrin <32302438+geanfeltrin@users.noreply.github.com>
2019-01-21 07:28:18 -0300

    test

---
+++ src/store/sagas/favorites.js
@@ -0,0 +1,39 @@
+import api from "../../services/api";
+
+import { call, put, select } from "redux-saga/effects";
+
+import { Creators as FavoriteActions } from "../ducks/favorites";
+
+export function* addFavorite(action) {
+    try {
+        const { data } = yield call(
+            api.get,
+            `/repos/${action.payload.repository}`
+        );
+
+        const isDuplicated = yield select(state =>
+            state.favorites.data.find(favorite => favorite.id === data.id)
+        );
+
+        if (isDuplicated) {
+            yield put(
+                FavoriteActions.addFavoriteFailure("Repositório Duplicado")
+            );
+        } else {
+            const repositoryData = {
+                id: data.id,
+                name: data.full_name,
+                description: data.description,
+                url: data.html_url
+            };
+
+            yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+        }
+    } catch (err) {
+        yield put(
+            FavoriteActions.addFavoriteFailure(
+                "erro ao adicionar o repositório"
+            )
+        );
+    }
+}
